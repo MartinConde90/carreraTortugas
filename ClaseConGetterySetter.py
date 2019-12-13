fe820568e8435c5242231ce799af37e183845c8d
MartinCG <martincondegrande@hotmail.com>
2019-12-13 03:27:28 +0100

    GetterYSetter

--- ClaseConGetterySetter.py
+++ ClaseConGetterySetter.py
@@ -8,7 +8,7 @@ class ClaseConGetterySetter():
     def getPropiedadPrivada(self):# Getter devuelve lo que hay dentro, al ser solo de lectura
         return self.__propiedad_privada
     
-    def propiedadPrivada(self, valor):
+    def propiedadPrivada(self, valor=None):# los mismo que set y get pero en uno
         if valor==None:
             #actua como getter
             return self.__propiedad_privada
@@ -29,7 +29,9 @@ c.setPropiedadPrivada('Un zorro con 7 colas')
 print(c)
 c.getPropiedadPrivada()
 print(c)
-
-
+c.propiedadPrivada('Brandon es color canela')
+print(c)
+c.getPropiedadPrivada()
+print(c)
 
 
